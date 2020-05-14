ace5ebe0d9eeab583ea11640e05fe1429e12d5de
xiazewei <xiazewei@blacklake.cn>
2020-05-14 18:58:06 +0800

    character_6

---
+++ Character_6/simple-controller-demo/src/test/kotlin/com/learn/simplecontrollerdemo/SimpleControllerDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.learn.simplecontrollerdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleControllerDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
